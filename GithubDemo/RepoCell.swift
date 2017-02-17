84850b6df27e0038fa23f8781efc17a2bd711e6a
my mac <mymac@my-macs-MacBook.local>
2017-02-16 21:11:05 -0800

    first commit

---
+++ GithubDemo/RepoCell.swift
@@ -0,0 +1,47 @@
+//
+//  RepoCell.swift
+//  GithubDemo
+//
+//  Created by my mac on 2/16/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+    
+    @IBOutlet weak var ownerImageView: UIImageView!
+    @IBOutlet weak var startCountLabel: UILabel!
+    @IBOutlet weak var repoNameLabel: UILabel!
+    @IBOutlet weak var repoOwnerLabel: UILabel!
+    @IBOutlet weak var forkCountLabel: UILabel!
+    @IBOutlet weak var repoDescriptionLabel: UILabel!
+    
+    
+    var repo : GithubRepo!{
+    
+        didSet{
+            ownerImageView.setImageWith(URL(string: repo.ownerAvatarURL!)!)
+            startCountLabel.text = "\(repo.stars!)"
+            repoNameLabel.text = repo.name!
+            repoOwnerLabel.text = "by \(repo.ownerHandle!)"
+            forkCountLabel.text = "\(repo.forks!)"
+            repoDescriptionLabel.text = repo.repoDescription!
+            
+            
+        
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GithubDemo/RepoResultsViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("View did load")
+        
+        //Initialize tableview
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //Dynamically calculate cell size
+        self.tableView.estimatedRowHeight  = self.tableView.rowHeight
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+        
+    }
+
+    // Perform the search.
+    fileprivate func doSearch() {
+        print("Called!")
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+            
+            self.repos = newRepos
+            // Print the returned repositories to the output window
+            for repo in newRepos {
+                print(repo)
+            }   
+
+            self.tableView.reloadData()
+            MBProgressHUD.hide(for: self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error)
+        })
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let repos = self.repos{
+            return repos.count
+
+        }
+        
+        return 0
+        
+    }
+    
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath) as! RepoCell
+        let repo = self.repos[indexPath.row]
+        cell.repo = repo
+        return cell
+        
+    }
+    
+}
+
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
